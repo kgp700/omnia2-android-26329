d485c745b9b85d72f020cc3fe64831d75581d3a0
kgp700 <neokim20@gmail.com>
2012-02-17 16:55:32 +0900

    test

--- build_kernel_Froyo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-. ./scripts/prepare_Froyo.sh prepare
-./scripts/build_kernel.sh r880
-. ./scripts/prepare_Froyo.sh cleanup
--- scripts/prepare_Ginger.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-[ -d ./initramfs   -a   -h ./initramfs ] && rm ./initramfs
-[ "$1" = "prepare" -a ! -d ./initramfs ] && ln -s ././initramfs-A23 ./initramfs
